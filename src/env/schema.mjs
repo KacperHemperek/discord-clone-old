c812859f6863e3540f5e00fc544e7de29a6fa4dd
KacperHemperek <kacper.hemperek@o2.pl>
2022-11-01 22:14:08 +0100

    move keys to env

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -8,6 +8,12 @@ import { z } from "zod";
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   FIREBASE_ADMIN: z.string(),
+  NEXT_PUBLIC_FIREBASE_API_KEY: z.string(),
+  NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: z.string(),
+  NEXT_PUBLIC_FIREBASE_PROJECT_ID: z.string(),
+  NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: z.string(),
+  NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: z.string(),
+  NEXT_PUBLIC_FIREBASE_APP_ID: z.string(),
   NODE_ENV: z.enum(["development", "test", "production"]),
 });
 
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCiyoi_G1qZ-GytHyW1bteatU8nON5Pvw4",
-  authDomain: "discord-clone-5b087.firebaseapp.com",
-  projectId: "discord-clone-5b087",
-  storageBucket: "discord-clone-5b087.appspot.com",
-  messagingSenderId: "193834427921",
-  appId: "1:193834427921:web:4a56f34fd58da3ddbff3b0",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 const app = initializeApp(firebaseConfig);
-
 
 export const auth = getAuth(app);
