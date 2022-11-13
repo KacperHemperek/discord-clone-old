993c092d3497f7f83036d07800b86582620ea245
KacperHemperek <kacper.hemperek@o2.pl>
2022-11-13 18:24:53 +0100

    add animation and fix loading styles

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -2,7 +2,7 @@ import { Message, User } from "@prisma/client";
 import React, { useEffect, useRef } from "react";
 import MessageComponent from "../Message/Message";
 import MessageSceleton from "../Message/MessageSceleton";
-import { useAuth } from "../UserProvider";
+import autoAnimate from "@formkit/auto-animate";
 
 type ChatProps = {
   messages:
@@ -13,20 +13,21 @@ type ChatProps = {
 
 function Chat({ messages, loading }: ChatProps) {
   const chatRef = useRef<HTMLDivElement>(null);
-  const { currentUser } = useAuth();
-
   useEffect(() => {
     if (chatRef.current) {
       chatRef.current?.scrollTo(0, chatRef.current.scrollHeight);
+      autoAnimate(chatRef.current);
     }
   }, []);
 
   return (
     <div
       ref={chatRef}
-      className="custom-scroll flex w-full flex-grow flex-col-reverse overflow-y-scroll  p-4 md:py-8 md:px-16"
+      className={`${
+        loading ? "overflow-y-hidden" : "overflow-y-scroll"
+      } custom-scroll flex w-full flex-grow flex-col-reverse   p-4 md:py-8 md:px-16`}
     >
-      {loading && !messages
+      {loading
         ? [...Array(10)].map((_, i) => <MessageSceleton key={i} />)
         : messages?.map(({ body, createdAt, id, user }) => (
             <MessageComponent
