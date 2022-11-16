63117c1b45e42a005fb12e44e895b38721b5dafc
KacperHemperek <kacper.hemperek@o2.pl>
2022-11-16 14:27:01 +0100

    update creating global prisma var

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,18 +1,22 @@
 // src/server/db/client.ts
 import { PrismaClient } from "@prisma/client";
-import { env } from "../../env/server.mjs";
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ["error"],
-  });
+// Avoid instantiating too many instances of Prisma in development
+// https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices#problem
+let prisma;
 
-if (env.NODE_ENV !== "production") {
-  global.prisma = prisma;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
+
+export default prisma;
