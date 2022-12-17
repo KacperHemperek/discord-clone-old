bc2ec0906e36ca42db4b04ba616d8a72b01d0629
KacperHemperek <kacper.hemperek@o2.pl>
2022-12-17 18:14:10 +0100

    change font on user card

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -6,7 +6,7 @@ function UserCard({ name, avatar }: { name: string; avatar?: string | null }) {
   return (
     <div className=" flex items-center">
       <Avatar avatar={avatar ?? PlaceholderAvatar} />
-      <p className="truncate whitespace-nowrap font-bold text-brandgray-100">
+      <p className="truncate whitespace-nowrap font-semibold text-brandgray-100">
         {name}
       </p>
     </div>
